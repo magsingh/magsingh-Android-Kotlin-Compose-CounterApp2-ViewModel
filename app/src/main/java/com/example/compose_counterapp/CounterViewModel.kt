234dac85fdd51da73bfd283403c6a2531a7e5e8a
Magendra Singh <Magendra@Singh.com>
2026-07-26 16:56:29 +0530

    Annotations

--- app/src/main/java/com/example/compose_counterapp/CounterViewModel.kt
+++ app/src/main/java/com/example/compose_counterapp/CounterViewModel.kt
@@ -7,16 +7,17 @@ import androidx.compose.runtime.setValue
 
 class CounterViewModel : ViewModel() {
     var count by mutableIntStateOf(0)
-    private set
-
+        private set //The UI should read the count.
+                    //Only the ViewModel should change it.
+                    //So make the setter private.
+                    //Now the UI can do: println(viewModel.count)
+                    //but cannot do: viewModel.count = 100
     fun increment() {
         count++
     }
-
     fun decrement() {
         count--
     }
-
     fun reset() {
         count = 0
     }
--- app/src/main/java/com/example/compose_counterapp/MainActivity.kt
+++ app/src/main/java/com/example/compose_counterapp/MainActivity.kt
@@ -4,33 +4,23 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.compose_counterapp.ui.theme.ComposeCounterAppTheme
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 class MainActivity : ComponentActivity() {
@@ -70,6 +60,7 @@ class MainActivity : ComponentActivity() {
                             Button(
                                 onClick = {
                                     counterViewModel.decrement()
+                                    //counterViewModel.count=500 //Gives error since count is private
                                     Log.d("MAGS", "- btn clicked")
                                 },
                                 modifier = Modifier.width(100.dp),
